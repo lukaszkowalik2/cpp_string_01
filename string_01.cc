055d11c9d1721e9706190427978c351f736f4239
Łukasz <lkowalik04@gmail.com>
2024-03-18 18:23:59 +0100

    finished func

--- string_01.cc
+++ string_01.cc
@@ -1,3 +1,31 @@
 #include "string_01.h"
 
-auto main() -> int { return 0; }
+#include <iostream>
+
+using namespace std;
+
+auto main() -> int {
+  string str = "12321 dsa 12 312 dsa dasd sasd";
+  string reverse_str = string_toys::reverse(str);
+
+  cout << "Reversed string: " << reverse_str << endl;
+
+  string str_first_to_up = string_toys::first_to_upper(str);
+
+  cout << "First to upper: " << str_first_to_up << endl;
+
+  int count_vowels = string_toys::count_vowels(str);
+  cout << "Vowels count: " << count_vowels << endl;
+
+  int digit_sum = string_toys::sum_digits(str);
+  cout << "Digit sum: " << digit_sum << endl;
+
+  string substr = "dsa 1";
+  int index = string_toys::search_substr(str, substr);
+  cout << "Substring index: " << index << endl;
+
+  int index2 = string_toys::custom_search(str.c_str(), substr.c_str());
+  cout << "Index 2:" << index2 << endl;
+
+  return 0;
+}
--- string_01.h
+++ string_01.h
@@ -1,33 +1,109 @@
+#include <cstring>
+#include <iostream>
+
 #ifndef CPP_STRING_H
 #define CPP_STRING_H
 
-
 namespace string_toys {
-    
-auto reverse(const std::string & str) -> std::string {
+auto uppercase(char c) -> char {
+  if (c >= 'a' && c <= 'z') {
+    c = c - 32;
+  }
 
+  return c;
 }
 
-auto first_to_upper(const std::string & str) -> std::string {
+auto reverse(const std::string& str) -> std::string {
+  std::string str_reversed;
 
+  for (const char& c : str) {
+    str_reversed = c + str_reversed;
+  }
+
+  return str_reversed;
 }
 
-auto count_vowels(const std::string & str) -> int {
-    
+auto first_to_upper(const std::string& str) -> std::string {
+  bool new_word = true;
+  std::string str_first_to_upper = str;
+
+  for (char& c : str_first_to_upper) {
+    if (new_word) {
+      c = uppercase(c);
+      new_word = false;
+    }
+    if (c == ' ') {
+      new_word = true;
+    }
+  }
+  return str_first_to_upper;
 }
 
-auto sum_digits(const std::string & str) -> int {
-    
+auto count_vowels(const std::string& str) -> int {
+  int count = 0;
+
+  for (const char& c : str) {
+    const char up_c = uppercase(c);
+    if (up_c == 'A' || up_c == 'E' || up_c == 'I' || up_c == 'O' || up_c == 'U' || up_c == 'Y') {
+      count++;
+    }
+  }
+  return count;
 }
 
-auto search_substr(const std::string & str, const std::string & substr) -> int {
-
+auto sum_digits(const std::string& str) -> int {
+  int sum = 0;
+  for (const char& c : str) {
+    bool is_digit = isdigit(c);
+    if (is_digit) {
+      sum = sum + c - '0';
+    }
+  }
+  return sum;
 }
 
-auto custom_serach(const char * str, const char * substr) -> int {
-    
+auto search_substr(const std::string& str, const std::string& substr) -> int {
+  int str_len = str.length();
+  int substr_len = substr.length();
+  int found_index = -1;
+
+  for (int i = 0; i <= str_len - substr_len; i++) {
+    bool match = true;
+    for (int j = 0; j < substr_len; j++) {
+      if (str[i + j] != substr[j]) {
+        match = false;
+        break;
+      }
+    }
+    if (match) {
+      found_index = i;
+      break;
+    }
+  }
+  return found_index;
 }
 
+auto custom_search(const char* str, const char* substr) -> int {
+  int str_len = strlen(str);
+  int substr_len = strlen(substr);
+  int found_index = -1;
+
+  for (int i = 0; i <= str_len - substr_len; i++) {
+    bool match = true;
+    for (int j = 0; j < substr_len; j++) {
+      if (*(str + i + j) != *(substr + j)) {
+        match = false;
+        break;
+      }
+    }
+    if (match) {
+      found_index = i;
+      break;
+    }
+  }
+  return found_index;
 }
+
+}  // namespace string_toys
 
 #endif /* CPP_STRING_H */
